8f8b4285c37804a1fa627bcbb95e43e8bfb5a4c1
Samuraiwarm <warm.pianist@gmail.com>
2019-07-14 00:26:33 +0700

    ✨ 🚧 add incomplete pokemon datamodel 少々お待ちください

---
+++ server/src/test-utils.ts
@@ -0,0 +1,47 @@
+import * as request from 'supertest';
+import { ISignupArgs } from './auth/auth.interfaces';
+import { createUserToPrisma, generateToken } from './auth/auth.resolvers';
+import { User, UserWhereUniqueInput } from './generated/prisma';
+import { db } from './server';
+import { graphqlServer, server } from './server';
+
+interface IVariables {
+	[key: string]: any;
+}
+
+export const mockUserArgs: ISignupArgs = {
+	userName: 'jakpat.m@gmail.com',
+	password: 'password123',
+	firstName: 'John',
+	lastName: 'Doe'
+};
+
+export const requestGql = (query: string, variables?: IVariables) => {
+	const body = JSON.stringify({
+		query,
+		variables
+	});
+	return request(server)
+		.post(graphqlServer.options.endpoint || '/')
+		.set('Content-Type', 'application/json')
+		.send(body);
+};
+
+export const createTestUserIfNotExist = async (args: ISignupArgs) => {
+	if (!(await db.exists.User({ userName: args.userName }))) {
+		await createUserToPrisma(db, args);
+	}
+};
+
+export const getUserBearerToken = async (userWhereUniqueInput: UserWhereUniqueInput) => {
+	const user = (await db.query.user({ where: userWhereUniqueInput })) as User;
+	return `Bearer ${generateToken(user)}`;
+};
+
+export const deleteTestUserIfExists = async (
+	userWhereUniqueInput: UserWhereUniqueInput
+) => {
+	if (await db.exists.User(userWhereUniqueInput)) {
+		await db.mutation.deleteUser({ where: userWhereUniqueInput });
+	}
+};
